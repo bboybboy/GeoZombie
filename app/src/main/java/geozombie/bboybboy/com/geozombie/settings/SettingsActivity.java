d4e282081c890d8664e0c5601837e8feaa93e6ac
bboybboy <kovalsky.v@gmail.com>
2017-04-19 16:01:37 +0300

    add keystore for google map api

--- app/src/main/java/geozombie/bboybboy/com/geozombie/settings/SettingsActivity.java
+++ app/src/main/java/geozombie/bboybboy/com/geozombie/settings/SettingsActivity.java
@@ -1,6 +1,31 @@
 package geozombie.bboybboy.com.geozombie.settings;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import geozombie.bboybboy.com.geozombie.R;
+
+public class SettingsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        init();
+    }
+
+    private void init() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
