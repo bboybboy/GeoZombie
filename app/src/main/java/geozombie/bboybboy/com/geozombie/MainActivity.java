5f07e4ca5f96cd622e2545c5c9999258e95f3023
bboybboy <kovalsky.v@gmail.com>
2017-04-19 12:53:46 +0300

    add settings activity

--- app/src/main/java/geozombie/bboybboy/com/geozombie/MainActivity.java
+++ app/src/main/java/geozombie/bboybboy/com/geozombie/MainActivity.java
@@ -1,7 +1,11 @@
 package geozombie.bboybboy.com.geozombie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import geozombie.bboybboy.com.geozombie.settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI() {
+        findViewById(R.id.settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSettingsActivity();
+            }
+        });
+    }
+
+    private void startSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/geozombie/bboybboy/com/geozombie/settings/SettingsActivity.java
@@ -0,0 +1,6 @@
+package geozombie.bboybboy.com.geozombie.settings;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+}
